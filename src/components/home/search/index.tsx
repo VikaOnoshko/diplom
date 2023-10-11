1a9bf50f24f6b30db3f718d584f0fbd4c962e735
vika <vika_onoshko@mail.ru>
2023-10-11 23:36:35 +0300

    add search component

--- src/components/home/search/index.tsx
+++ src/components/home/search/index.tsx
@@ -1,14 +1,51 @@
 import './index.less';
+import { useState, useEffect } from 'react';
+import { ProductService } from '@services/product.service';
+import { EmptySearchResult } from './empthy-result';
+import { SearchResultProduct } from './search-result';
 
-export const HomeSeach = () => {
+export const HomeSearch = () => {
+  const [focus, setFocus] = useState(false);
+  const [query, setQuery] = useState('');
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    if (query) {
+      ProductService.getList({ search: query, page: 1, limit: 5 }).then(
+        (data) => setProducts(data.items),
+      );
+    }
+  }, [query]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
   return (
-    <section className="search">
-      <div className="search__container">
-        <div className="search__wrapper">
-          <div className="search__input-box">
-            <input type="text" placeholder="Поиск по товарам" />
+    <section className="main-search">
+      <div className="main-search__container">
+        <div className="main-search__wrapper">
+          <div className="main-search__input-box">
+            <input
+              type="text"
+              placeholder="Поиск по товарам"
+              value={query}
+              onChange={handleChange}
+              onFocus={() => setFocus(true)}
+              onBlur={() => setFocus(false)}
+            />
             <span className="icon-search"></span>
           </div>
+          {(true || focus) &&
+            (products.length > 0 ? (
+              <div className="main-search__result-list">
+                {products.map((product) => (
+                  <SearchResultProduct key={product.id} product={product} />
+                ))}
+              </div>
+            ) : (
+              <EmptySearchResult />
+            ))}
         </div>
       </div>
     </section>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,12 +4,12 @@ import { HomeBanner } from '@components/home/banner';
 import { HomeForClient } from '@components/home/for-client';
 import { ProductList } from '@components/home/product-list';
 import { HomeNewsList } from '@components/home/news-list';
-import { HomeSeach } from '@components/home/search';
+import { HomeSearch } from '@components/home/search';
 
 export const HomePage = () => {
   return (
     <div className="home">
-      <HomeSeach />
+      <HomeSearch />
       <HomeBanner />
       <HomeForClient />
       <ProductList title="Акции" filter={{ onSale: true }} />
