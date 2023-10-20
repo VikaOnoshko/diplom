71776221f5c7d70e6976d9c87c41ecc3ffdc4b82
vika <vika_onoshko@mail.ru>
2023-10-20 23:23:59 +0300

    add sort button

--- src/components/catalog/sort/index.tsx
+++ src/components/catalog/sort/index.tsx
@@ -2,6 +2,15 @@ import './index.less';
 import { Select } from '@ui/shared/select';
 import { useAppDispath, useAppSelector } from '@redux/store/store';
 import { setSort } from '@redux/reducers/catalog.reducer';
+import { Button } from '@ui/shared/button';
+import { useState } from 'react';
+
+const SORT = [
+  { value: 'rating', name: 'Рейтинг по возрастанию' },
+  { value: '-rating', name: 'Рейтинг по убыванию' },
+  { value: 'price', name: 'Цена по возрастанию' },
+  { value: '-price', name: 'Цена по убыванию' },
+];
 
 export const Sort = () => {
   const { sort } = useAppSelector((state) => state.catalog);
@@ -11,17 +20,43 @@ export const Sort = () => {
     dispatch(setSort(value));
   };
 
+  const sortLabel =
+    SORT.find((item) => item.value === sort)?.name || 'Не выбран';
+
+  const [isOptions, setIsOptions] = useState(false);
+
   return (
-    <Select
-      value={sort}
-      onChange={handleChange}
-      title={'Сортировка'}
-      options={[
-        { value: 'rating', name: 'Рейтинг по возрастанию' },
-        { value: '-rating', name: 'Рейтинг по убыванию' },
-        { value: 'price', name: 'Цена по возрастанию' },
-        { value: '-price', name: 'Цена по убыванию' },
-      ]}
-    />
+    <>
+      <Select
+        className="select"
+        value={sort}
+        onChange={handleChange}
+        title={'Сортировка'}
+        options={SORT}
+      />
+      <div className="button__filter">
+        <Button
+          text={sortLabel}
+          onClick={() => {
+            setIsOptions(!isOptions);
+          }}
+        />
+
+        {isOptions && (
+          <div
+            className="button__options-list"
+            onClick={() => {
+              isOptions;
+            }}
+          >
+            {SORT.map((item) => (
+              <div className="button__optons__item">
+                <span>{item.name}</span>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   );
 };
